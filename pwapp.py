303a6efcc01c9bfe8d52f418c3ee9feffb9e344b
manystarsapart <manystarsapart@gmail.com>
2024-07-04 09:22:04 +0800

    make working grass tabulator and link to gacha pull simulator

--- pwapp.py
+++ pwapp.py
@@ -12,25 +12,36 @@ if "totalPulledCount" not in st.session_state:
     st.session_state.totalPulledCount = 0
 if "hard_pity" not in st.session_state:
     st.session_state.hard_pity = 0
+if "grassCount" not in st.session_state:
+    st.session_state.grassCount = 0
 
 #col1: data input
 
-# targetScreenTime = float(col1.text_input("target screen time by the end of 4 weeks"))
-# pastScreenTime = float(col1.text_input("actual average screen time from phone for the past week"))
+targetScreenTime = col1.text_input("target screen time by the end of 4 weeks")
+pastScreenTime = col1.text_input("actual average screen time from phone for the past week")
 
-# col1.write("your target is: " + str(targetScreenTime) + " hours")
-# col1.write("for the last week, your screen time was: " + str(pastScreenTime) + " hours")
+col1.write("your target is: " + str(targetScreenTime) + " hours")
+col1.write("for the last week, your screen time was: " + str(pastScreenTime) + " hours")
 
-# col1.html("<hr>", unsafe_allow_html=True) # draws a line
+col1.html("<hr>") # draws a line
 
-# todayScreenTime = float(col1.text_input("screen time by the end of today"))
-# col1.write("today's screen time: " + str(todayScreenTime))
-# col1.write("past screen time: " + str(pastScreenTime))
-# if todayScreenTime <= pastScreenTime:
-#     col1.write("congrats! your screen time today is under your past screen time.")
-    
+todayScreenTime = col1.text_input("screen time by the end of today")
+col1.write("today's screen time: " + str(todayScreenTime))
+col1.write("past screen time: " + str(pastScreenTime))
 
-#col2: the gacha simulator
+if col1.button("tabulate grass"):
+    # if todayScreenTime is lower than pastScreenTime, earn that much grass. otherwise grass remains the same
+    if pastScreenTime and todayScreenTime:
+        todayScreenTime, pastScreenTime = int(todayScreenTime), int(pastScreenTime)
+        if todayScreenTime <= pastScreenTime:
+            col1.write("congrats! your screen time today is under your past screen time.")
+            st.session_state.grassCount += (pastScreenTime - todayScreenTime)
+            col1.write("you now have " + str(st.session_state.grassCount) + " grass!")
+        elif todayScreenTime > pastScreenTime:
+            col1.write("please do better... ")
+
+
+# col2: the gacha simulator
 
 col2.header("gacha pull simulator")  
 
@@ -40,14 +51,7 @@ limited = "qi"
 standard = "not_qi"
 guaranteed_status = 0
 max_pity = 80
-
-
-
-
-
-
-
-
+grass_per_pull = 1
 
 def gacha_pull():
     if st.session_state.hard_pity < max_pity: # normal pull, not hard pity
@@ -64,15 +68,7 @@ def gacha_pull():
         st.session_state.hard_pity = 0 
     st.session_state.totalPulledCount += 1
     st.session_state.inventory.append(gacha_result)
-        
-        
-
-
-
-
-
-
-
+  
 
 def clear_inv():
     st.session_state.inventory = []
@@ -98,26 +94,39 @@ def view_gacha_stats():
 def check_inv():
     col2.write(st.session_state.inventory)
 
- 
+
+# display in col2
+if st.session_state.grassCount >= grass_per_pull:
+    if col2.button("hide dialogue"):
+        col2.write()
+
+if st.session_state.grassCount >= grass_per_pull:
+    if col2.button("pull 1"):
+        gacha_pull()
+        col2.write("you pulled 1 time")
+        st.session_state.grassCount -= grass_per_pull
+
+if st.session_state.grassCount >= 10*grass_per_pull:
+    if col2.button("pull 10"):
+        pull_for_amt(10)
+        st.session_state.grassCount -= 10*grass_per_pull
+
+if st.session_state.grassCount >= 100*grass_per_pull:
+    if col2.button("pull 100"):
+        pull_for_amt(100)
+        st.session_state.grassCount -= 100*grass_per_pull
+
+if st.session_state.grassCount >= 1000*grass_per_pull:
+    if col2.button("pull 1000"):
+        pull_for_amt(1000)
+        st.session_state.grassCount -= 1000*grass_per_pull
 
 
+col2.write("grass count: " + str(st.session_state.grassCount))
 
-if col2.button("hide dialogue"):
-    #balls
-    col2.write()
+view_gacha_stats()
 
-if col2.button ("pull 1"):
-    gacha_pull()
-    col2.write("you pulled 1 time")
-
-if col2.button("pull 10"):
-    pull_for_amt(10)
-
-if col2.button("pull 100"):
-    pull_for_amt(100)
-
-if col2.button("pull 1000"):
-    pull_for_amt(1000)
+col2.html("<hr>") 
 
 if col2.button("check inventory"):
     check_inv()
@@ -125,5 +134,3 @@ if col2.button("check inventory"):
 if col2.button ("clear inventory"):
     clear_inv()
     col2.write("inventory cleared")
-
-view_gacha_stats()
